7150a1b3995ad5adedca13be0d98cfa6f3d64fc1
wangwei <wang_wei608@163.com>
2018-12-17 18:02:48 +0800

    服务网关zuul的init

---
+++ src/main/java/com/sell/apigateway/ZuulConfig.java
@@ -0,0 +1,23 @@
+package com.sell.apigateway;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.zuul.filters.ZuulProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author WangWei
+ * @Title: ZuulConfig
+ * @ProjectName api-gateway
+ * @date 2018/12/17 15:18
+ * @description:
+ */
+/*@Component
+public class ZuulConfig {
+
+    @ConfigurationProperties("zuul")
+    @RefreshScope
+    public ZuulProperties zuulProperties() {
+        return new ZuulProperties();
+    }
+}*/
